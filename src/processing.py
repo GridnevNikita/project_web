64b155c228f85e35722f0a0cd8b19922737090c7
nikita gridnev <fox-niks@yandex.ru>
2025-05-08 16:27:35 +0300

    add processing.py func sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -2,6 +2,10 @@ def filter_by_state(data, state='EXECUTED'):
     return [item for item in data if item['state'] == state]
 
 
+def sort_by_date(data, reverse=True):
+    return [item for item in sorted(data, key=lambda x: x['date'], reverse=reverse)]
+
+
 if __name__ == "__main__":
     filtered_data = filter_by_state([
         {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
@@ -11,4 +15,15 @@ if __name__ == "__main__":
     ])
 
     for item in filtered_data:
+        print(item)
+
+
+    sorted_data = sort_by_date([
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    ])
+
+    for item in sorted_data:
         print(item)
