b3400f537e988aa6714c1a53fcd6783f6b77a7e2
nikita gridnev <fox-niks@yandex.ru>
2025-05-25 18:36:53 +0300

    add logging utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,17 @@
 import json
 import os
 from typing import Any, List
+import logging
+
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+log_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "logs", "utils.log")
+file_handler = logging.FileHandler(log_path, encoding="utf-8")
+file_handler.setLevel(logging.DEBUG)
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def load_json_file(file_path: str) -> List[Any]:
@@ -10,13 +21,20 @@ def load_json_file(file_path: str) -> List[Any]:
     или содержимое не является списком.
     """
     if not os.path.isfile(file_path):
+        logger.warning(f"Файл не найден: {file_path}")
         return []
 
     try:
         with open(file_path, "r", encoding="utf-8") as file:
             data = json.load(file)
-            return data if isinstance(data, list) else []
-    except (FileNotFoundError, json.JSONDecodeError):
+            if isinstance(data, list):
+                logger.debug(f"Успешно загружен список из файла: {file_path}")
+                return data
+            else:
+                logger.warning(f"Файл не содержит список: {file_path}")
+                return []
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        logger.error(f"Ошибка при загрузке JSON-файла: {e}")
         return []
 
 
